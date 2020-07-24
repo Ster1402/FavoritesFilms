c5350b4e1813c6804d5656b69b0346a2de574842
NDE TSAPI STEVE-ROLAND <56319959+Ster1402@users.noreply.github.com>
2020-07-24 13:39:46 +0100

    Update Navigation.js

--- Navigation.js
+++ Navigation.js
@@ -1,21 +1,15 @@
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
 import Search from '../Components/Search';
 
 const StackNavigator = createStackNavigator();
 
-class Navigation extends React.Component {
-    render() {
-        return (
-            <NavigationContainer>
-			<StackNavigator.Navigator>
-				<StackNavigator.Screen name="Search" component={Search} />
-			</StackNavigator.Navigator>
-		</NavigationContainer>
-        )
-    }
+const Navigation = () => {
+   return (<NavigationContainer>
+        <StackNavigator.Navigator>
+            <StackNavigator.Screen name="Rechercher" component={Search}/>
+        </StackNavigator.Navigator>
+    </NavigationContainer>)
 }
 
-export default Navigation;
+export default Navigation
