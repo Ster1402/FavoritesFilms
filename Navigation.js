c0bbef2e1ce0c44274fe3c754f390cd983471587
NDE TSAPI STEVE-ROLAND <56319959+Ster1402@users.noreply.github.com>
2020-07-24 19:28:50 +0100

    Update Navigation.js

--- Navigation.js
+++ Navigation.js
@@ -1,15 +1,12 @@
-import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/stack'
-import Search from '../Components/Search';
+import { createStackNavigator } from '@react-navigation/stack';
+import Search from '../Components/Search'
 
-const StackNavigator = createStackNavigator();
+export const Stack = createStackNavigator();
 
-const Navigation = () => {
-   return (<NavigationContainer>
-        <StackNavigator.Navigator>
-            <StackNavigator.Screen name="Rechercher" component={Search}/>
-        </StackNavigator.Navigator>
-    </NavigationContainer>)
+export default function Navigation() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Rechercher" component={Search}/>
+    </Stack.Navigator>
+  );
 }
-
-export default Navigation
