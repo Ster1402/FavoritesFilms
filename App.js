9dbf5fde8a9d1d607cbc7f7b35b7e2742b9ee11d
NDE TSAPI STEVE-ROLAND <56319959+Ster1402@users.noreply.github.com>
2020-07-24 19:28:04 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import Navigation from './Navigation/Navigation';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Navigation from './Navigation/Navigation'
 
-class App extends React.Component {
-	render() {
-		return <Navigation />;
-	}
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
+  );
 }
-
-export default App;
